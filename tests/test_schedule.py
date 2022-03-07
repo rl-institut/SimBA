e33fabb0997285856c0efd85fad14ed7774dda63
Paul Boehnke <paul.boehnke@rl-institut.de>
2022-03-07 15:28:43 +0100

    removed dummy test

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -6,10 +6,3 @@ https://github.com/rl-institut/workshop/tree/master/test-driven-development.
 Otherwise https://docs.pytest.org/en/latest/ and https://docs.python.org/3/library/unittest.html
 are also good support.
 """
-from ebus_toolbox import schedule
-
-
-def test_schedule_from_csv():
-    s = schedule.Schedule.from_csv('./data/private_examples/trips_example-bvg_datetime.csv',
-                                   './data/examples/vehicle_types.json')
-    assert len(s.rotations) == 6
