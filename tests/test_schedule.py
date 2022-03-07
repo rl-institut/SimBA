66e76feb17eebf845a876b47dc8559d710645f09
Paul Boehnke <paul.boehnke@rl-institut.de>
2022-03-07 15:31:47 +0100

    single dummy test is necessary

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -6,3 +6,9 @@ https://github.com/rl-institut/workshop/tree/master/test-driven-development.
 Otherwise https://docs.pytest.org/en/latest/ and https://docs.python.org/3/library/unittest.html
 are also good support.
 """
+from ebus_toolbox import schedule
+
+
+def test_schedule_from_csv():
+    empty_schedule = schedule.Schedule('./data/examples/vehicle_types.json')
+    assert empty_schedule.rotations == {}
