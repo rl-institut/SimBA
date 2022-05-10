f4c49b9a762de1f6df14b81699bdcec633cac7f8
timroepcke <tim.roepcke@rl-institut.de>
2022-05-10 11:34:42 +0200

    add blank function

---
+++ ebus_toolbox/sensitivity_analysis.py
@@ -0,0 +1,7 @@
+""" Optimizer that evaluates inputs and outputs of every iteration, adapting scenario setup
+    to optimize for specified metrics.
+"""
+
+
+def no_optimization():
+    return "converged"
