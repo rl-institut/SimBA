25ef3b757d9c4b3c44402ec81eafadf3a53ce2ea
yplatow <yannick.platow@rl-institut.de>
2022-06-21 10:55:06 +0200

    weather data added, random values from normal distribution

--- ebus_toolbox/sensitivity_analysis.py
+++ ebus_toolbox/sensitivity_analysis.py
@@ -4,6 +4,7 @@ import scipy
 from scipy.stats import gamma
 from ebus_toolbox.util import read_arguments
 import pandas as pd
+import numpy as np
 
 # define sturgeon values
 
@@ -18,7 +19,7 @@ list_r = []
 for i in range(delay_data.shape[0]):
     gamma_a = delay_data.loc[i, 'gamma_a']
     if delay_data.loc[i, 'gamma_scale'] < 0:
-        gamma_scale = delay_data.loc[i, 'gamma_scale']*-1
+        gamma_scale = delay_data.loc[i, 'gamma_scale']*0
     else:
         gamma_scale = delay_data.loc[i, 'gamma_scale']
     r = scipy.stats.gamma.rvs(gamma_a, loc=0, scale=gamma_scale, size=1)
@@ -27,46 +28,40 @@ for i in range(delay_data.shape[0]):
 delay_data['r'] = list_r
 
 # averaging of values over hour of the day
-means = delay_data.groupby('hour').mean()
+delay = delay_data.groupby('hour')['r'].mean()
 
 # create dictionary for cfg file 
-delay = {'0': means.r[0],
-         '1': means.r[1],
-         '2': means.r[2],
-         '3': means.r[3],
-         '4': means.r[4],
-         '5': means.r[5],
-         '6': means.r[6],
-         '7': means.r[7],
-         '8': means.r[8],
-         '9': means.r[9],
-         '10': means.r[10],
-         '11': means.r[11],
-         '12': means.r[12],
-         '13': means.r[13],
-         '14': means.r[14],
-         '15': means.r[15],
-         '16': means.r[16],
-         '17': means.r[17],
-         '18': means.r[18],
-         '19': means.r[19],
-         '20': means.r[20],
-         '21': means.r[21],
-         '22': means.r[22],
-         '23': means.r[23],
-         'else': 2
-         }
+
+delay = delay.to_dict()
+
+# 2. temperature
+
+# reading data
+weather_data = pd.read_csv('data/monte_carlo/produkt_tu_stunde_19510101_20211231_00433.csv', sep=';', decimal='.')
+
+weather_data['MESS_DATUM'] = weather_data['MESS_DATUM'].astype('str')
+
+for i in range(weather_data.shape[0]):
+    weather_data.loc[i, 'year'] = weather_data.loc[i, 'MESS_DATUM'][:4]
+    weather_data.loc[i, 'month'] = weather_data.loc[i, 'MESS_DATUM'][4:6]
+    weather_data.loc[i, 'day'] = weather_data.loc[i, 'MESS_DATUM'][6:8]
+    weather_data.loc[i, 'hour'] = weather_data.loc[i, 'MESS_DATUM'][8:10]
+
+weather_data['Date'] = pd.to_datetime(weather_data[['year', 'month', 'day', 'hour']])
+weather_data = weather_data[weather_data['year'] == '2021']
+weather = pd.DataFrame(weather_data.groupby('hour')['TT_TU'].mean())
+weather = weather.to_dict()
+
+# generate random values from normal distribution
+mean_2 = weather_data.mean()
+
+mu = mean_2.TT_TU  # mean
+sigma = weather_data['TT_TU'].std()  # standard deviation
+s = np.random.normal(mu, sigma, 1)
 
 # configs erstellen
 
-
-def read_cfg():
-    args = read_arguments()
-    return "converged"
-
-
-if __name__ == '__main__':
-    read_cfg()
+args = read_arguments()
 
 # config manipulieren/ergänzen
 
