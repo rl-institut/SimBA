b7f85258de8bb510acb4ca7de0adbdbd034a1bda
Julian Brendel <Julian.Brendel@rl-institut.de>
2022-09-01 16:51:43 +0200

    cost function: rename several parameters and how vehicle types are accessed

--- ebus_toolbox/costs.py
+++ ebus_toolbox/costs.py
@@ -19,75 +19,93 @@ def calculate_costs(args, schedule):
         el_stations = json.load(json_file)
 
     # VEHICLES
-    c_busses = 0
+    c_vehicles = 0
+    c_vehicles_annual = 0
     v_types = schedule.scenario["constants"]["vehicle_types"]
     for v_type, v_keys in v_types.items():
         if schedule.vehicle_type_counts[v_type] > 0:
-            if "solo" in v_keys["name"]:
-                costs_bus = c_params["bus"]["solo_bus"]
-            elif "articulated" in v_keys["name"]:
-                costs_bus = c_params["bus"]["articulated_bus"]
+            try:
+                costs_vehicle = c_params["vehicles"][v_type.split("_")[0]]["capex"]
+                # future solution -> costs_vehicle= c_params["vehicles"][v_type]["capex"]
+            except KeyError:
+                print("Warning: No capex defined for vehicle type " + v_type.split("_")[0] +
+                      ". Unable to calculate investment costs for this vehicle type.")
             else:
-                costs_bus = c_params["bus"]["battery_kwh"] = 0
-                print("Warning: Name of vehicle_type doesn't include 'solo' or 'articulated. "
-                      "Unable to calculate investment and maintenance costs for busses.'")
-            # sum up cost of busses and its batteries, depending on how often the battery has to be
-            # replaced in the lifespan of the busses
-            c_busses += (schedule.vehicle_type_counts[v_type] *
-                         (costs_bus - (-c_params["bus"]["lifespan_bus"]
-                                       // c_params["bus"]["lifespan_battery"]) *
-                          v_keys["capacity"] * c_params["bus"]["battery_kwh"]))
-    # calculate annual cost of busses, depending on its lifespan
-    c_busses_annual = round(c_busses / c_params["bus"]["lifespan_bus"], ROUND_TO_PLACES)
+                # sum up cost of vehicles and their batteries, depending on how often the battery
+                # has to be replaced in the lifetime of the vehicles
+                c_vehicles_vt = schedule.vehicle_type_counts[v_type] * \
+                                (costs_vehicle - (-c_params["vehicles"][v_type.split("_")[0]]["lifetime"]
+                                                 // c_params["batteries"]["lifetime_battery"]) *
+                                v_keys["capacity"] * c_params["batteries"]["cost_per_kWh"])
+                c_vehicles += c_vehicles_vt
+                # calculate annual cost of vehicles of this type, depending on their lifetime
+                c_vehicles_annual +=round(c_vehicles_vt/
+                                          c_params["vehicles"][v_type.split("_")[0]]["lifetime"],
+                                           ROUND_TO_PLACES)
+
 
     # GRID CONNECTION POINTS
     c_gcs = 0
+    c_gcs_annual = 0
     gcs = schedule.scenario["constants"]["grid_connectors"]
     for gcID, gc_keys in gcs.items():
-        c_gcs += c_params["gc"]["building_cost_subsidy_per_kW"] * gc_keys["max_power"] + \
-                 c_params["gc"]["transformer"] + c_params["gc"]["fix"] + \
-                 c_params["gc"]["variable"] * el_stations[gcID]["distance_transformer"]
-    # calculate annual cost of grid connectors, depending on its lifespan
-    c_gcs_annual = round(c_gcs / c_params["gc"]["lifespan_gc"], ROUND_TO_PLACES)
+        try:
+            distance_transformer = el_stations[gcID]["distance_transformer"]
+        except KeyError:
+            distance_transformer = c_params["gc"]["default_distance"]
+        c_gc = c_params["gc"]["building_cost_subsidy_per_kW"] * gc_keys["max_power"] + \
+               c_params["gc"]["capex_gc_fix"] + \
+               c_params["gc"]["capex_gc_per_meter"] * distance_transformer
+        c_transformer = c_params["gc"]["capex_transformer_fix"] + \
+                        c_params["gc"]["capex_transformer_per_kW"] * gc_keys["max_power"]
+        c_gcs += c_gc + c_transformer
+
+        # calculate annual costs of grid connectors, depending the lifetime of gc and transformator
+        c_gcs_annual += round(c_gc / c_params["gc"]["lifetime_gc"] +
+                             c_transformer / c_params["gc"]["lifetime_transformer"],
+                             ROUND_TO_PLACES)
 
     # CHARGING INFRASTRUCTURE
     c_cs = 0
     cs = schedule.scenario["constants"]["charging_stations"]
     for csID in cs.values():
         if csID["type"] == "deps":
-            c_cs += c_params["cs"]["depot_cs_per_kW"] * csID["max_power"]
+            c_cs += c_params["cs"]["capex_deps_per_kW"] * csID["max_power"]
         elif csID["type"] == "opps":
-            c_cs += c_params["cs"]["opportunity_cs_per_kW"] * csID["max_power"]
-    # calculate annual cost of charging stations, depending on its lifespan
-    c_cs_annual = round(c_cs / c_params["cs"]["lifespan_cs"], ROUND_TO_PLACES)
+            c_cs += c_params["cs"]["capex_opps_per_kW"] * csID["max_power"]
+    # calculate annual cost of charging stations, depending on their lifetime
+    c_cs_annual = round(c_cs / c_params["cs"]["lifetime_cs"], ROUND_TO_PLACES)
 
     # GARAGE
     c_garage_cs = (c_params["garage"]["n_charging_stations"] * c_params["garage"]["power_cs"] *
-                   c_params["cs"]["depot_cs_per_kW"])
-    c_garage_positions = -(-sum(schedule.vehicle_type_counts.values()) //
-                           c_params["garage"]["busses_per_positions"] *
-                           c_params["garage"]["cost_per_position"])
-    c_garage = c_garage_cs + c_garage_positions
-    # ToDo: annual costs of garage?
-
+                   c_params["cs"]["capex_deps_per_kW"])
+    c_garage_workstations = -(-sum(schedule.vehicle_type_counts.values()) //
+                              c_params["garage"]["vehicles_per_workstation"] *
+                              c_params["garage"]["cost_per_workstation"])
+    c_garage = c_garage_cs + c_garage_workstations
+    c_garage_annual = round(c_garage_cs / c_params["cs"]["lifetime_cs"]
+                            + c_garage_workstations / c_params["garage"]["lifetime_workstations"]
+                            , ROUND_TO_PLACES)
     # MAINTENANCE
-    m_infra = c_cs * c_params["maintenance"]["percentage_of_cs_infra"]
-    m_bus = 0
+    m_infra = c_cs * c_params["cs"]["c_maint_cs_per_year"] + \
+              c_transformer * c_params["gc"]["c_maint_transformer_per_year"]
+    m_vehicles = 0
     # calculate (ceil) number of days in scenario
     drive_days = -(-(schedule.scenario["scenario"]["n_intervals"] *
                      schedule.scenario["scenario"]["interval"]) // (24 * 60))
     for rot in schedule.rotations.values():
-        vt_ct = f"{rot.vehicle_type}_{rot.charging_type}"
-        if "solo" in v_types[vt_ct]["name"]:
-            m_bus += (rot.distance / 1000 / drive_days * 365) * \
-                     c_params["maintenance"]["solo_bus_per_km"]
-        elif "articulated" in v_types[vt_ct]["name"]:
-            m_bus += (rot.distance / 1000 / drive_days * 365) * \
-                     c_params["maintenance"]["articulated_bus_per_km"]
-    c_maintenance_annual = round(m_infra + m_bus, ROUND_TO_PLACES)
+        try:
+            m_vehicles += (rot.distance / 1000 / drive_days * 365) * \
+                          c_params["vehicles"][rot.vehicle_type]["c_maint_per_km"]
+        except KeyError:
+            print("Warning: No maintanance costs defined for vehicle type " +
+                  c_params["vehicles"][rot.vehicle_type] +
+                  ". Unable to calculate maintanance costs for this vehicle type.")
 
-    c_invest = c_busses + c_cs + c_gcs + c_garage
-    c_invest_annual = c_busses_annual + c_cs_annual + c_gcs_annual
+    c_maintenance_annual = round(m_infra + m_vehicles, ROUND_TO_PLACES)
+    c_invest = c_vehicles + c_cs + c_gcs + c_garage
+    c_invest_annual = round(c_vehicles_annual + c_cs_annual + c_gcs_annual + c_garage_annual,
+                            ROUND_TO_PLACES)
 
     return {"c_invest": c_invest, "c_invest_annual": c_invest_annual,
             "c_maintenance_annual": c_maintenance_annual}
