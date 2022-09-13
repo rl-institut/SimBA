54c7661cc10310bff30f2412610b6ed1b382cc38
paul.scheer <Paul.Scheer@rl-institut.de>
2022-09-13 12:10:12 +0200

    Change for flake8

--- ebus_toolbox/consumption.py
+++ ebus_toolbox/consumption.py
@@ -14,7 +14,8 @@ class Consumption:
             for row in reader:
                 self.temperatures_by_hour.update({int(row['hour']): float(row['temperature'])})
 
-        with open(kwargs.get("level_of_loading_over_day", "data/examples/default_level_of_loading_over_day.csv")) as f:
+        with open(kwargs.get("level_of_loading_over_day",
+                             "data/examples/default_level_of_loading_over_day.csv")) as f:
             reader = csv.DictReader(f)
             self.lol_by_hour = {}
             for row in reader:
@@ -24,7 +25,7 @@ class Consumption:
         self.vehicle_types = vehicle_types
 
     def calculate_consumption(self, time, distance, vehicle_type, charging_type,
-                              height_difference=0, level_of_loading=None, mean_speed=18):
+                              height_diff=0, level_of_loading=None, mean_speed=18):
         """ Calculates consumed amount of energy for a given distance.
 
         :param time: The date and time at which the trip ends
@@ -36,14 +37,14 @@ class Consumption:
         :param charging_type: Charging type for the trip. Consumption differs between
                               distinct types.
         :type charging_type: str
-        :param height_difference: difference in height between stations in meters-
-        :type height_difference: float
-        :param level_of_loading: Level of loading of the bus between empty (=0) and completely full (=1.0). If None
+        :param height_diff: difference in height between stations in meters-
+        :type height_diff: float
+        :param level_of_loading: Level of loading of the bus between empty (=0) and
+        completely full (=1.0). If None
         is provided, Level of loading will be interpolated from timeseries
         :type level_of_loading: float
         :param mean_speed: Mean speed between two stops in km/h
         :type mean_speed: float
-
         :return: Consumed energy [kWh] and delta SOC as tuple
         :rtype: (float, float)
         """
@@ -76,15 +77,15 @@ class Consumption:
         # load consumption csv
         consumption_file = self.vehicle_types[vt_ct]["mileage"]
 
-        # Consumption_files holds interpol functions of csv files which are called directly and held in memory
+        # Consumption_files holds interpol functions of csv files which are called directly
         vehicle_type_nr = dict(SB=0, VDL=0, AB=1, CKB=1)[vehicle_type]
         try:
             mileage = self.consumption_files[consumption_file](this_vehicle_type=vehicle_type_nr,
-                                                               this_incline=height_difference / distance,
+                                                               this_incline=height_diff / distance,
                                                                this_temp=temp,
                                                                this_lol=level_of_loading,
                                                                this_speed=mean_speed)
-            consumed_energy= mileage * distance
+            consumed_energy = mileage * distance
             delta_soc = -1 * (consumed_energy / self.vehicle_types[vt_ct]["capacity"])
             return consumed_energy, delta_soc
         except KeyError:
@@ -96,16 +97,21 @@ class Consumption:
             lol_u = list(df["level_of_loading"].unique())
             speed_u = list(df["mean_speed_kmh"].unique())
             points = (vehicle_type_u, incline_u, temp_u, lol_u, speed_u)
-            values = np.zeros((len(vehicle_type_u), len(incline_u), len(temp_u), len(lol_u), len(speed_u)))
+            values = np.zeros((len(vehicle_type_u), len(incline_u),
+                               len(temp_u), len(lol_u), len(speed_u)))
 
             # Nested loops for creating values in grid
             for i, vt in enumerate(vehicle_type_u):
+                mask_vt = df["vehicle_type"] == vt
                 for ii, inc in enumerate(incline_u):
+                    mask_inc = df["incline"] == inc
                     for iii, temp in enumerate(temp_u):
+                        mask_temp = df["temp"] == temp
                         for iv, lol in enumerate(lol_u):
+                            mask_lol = df["level_of_loading"] == lol
                             for v, speed in enumerate(speed_u):
-                                values[i, ii, iii, iv, v] = df[(df["vehicle_type"] == vt) * (df["incline"] == inc) *
-                                                               (df["temp"] == temp) * (df["level_of_loading"] == lol) *
+                                values[i, ii, iii, iv, v] = df[mask_vt * mask_inc *
+                                                               mask_temp * mask_lol *
                                                                (df["mean_speed_kmh"] == speed)][
                                     "consumption_kwh_per_km"]
 
@@ -116,7 +122,7 @@ class Consumption:
             self.consumption_files.update({consumption_file: interpol_function})
 
         mileage = self.consumption_files[consumption_file](this_vehicle_type=vehicle_type_nr,
-                                                           this_incline=height_difference / distance,
+                                                           this_incline=height_diff / distance,
                                                            this_temp=temp,
                                                            this_lol=level_of_loading,
                                                            this_speed=mean_speed)
