cc6a6da7a2b793cc712de658a17b6763ff1d8053
lilAndy-bruh <alexkens93@gmail.com>
2022-11-28 15:55:47 +0100

    Add new function plot, basically copied from spiceEV #39

--- ebus_toolbox/report.py
+++ ebus_toolbox/report.py
@@ -4,8 +4,9 @@ import csv
 import datetime
 import warnings
 import json
+import matplotlib.pyplot as plt
 
-from src.report import aggregate_timeseries, aggregate_local_results
+from src.report import aggregate_timeseries, aggregate_local_results, aggregate_global_results
 
 
 def generate_vehicle_socs(scenario, args):
@@ -61,6 +62,97 @@ def generate_cs_power_overview(scenario, args):
             csv_writer.writerow((i, t, ) + tuple(row))
 
 
+def plot(scenario, args):
+    print('Done. Create plots...')
+
+    xlabels = []
+    for r in scenario.results:
+        xlabels.append(r['current_time'])
+
+    # batteries
+    if scenario.batteryLevels:
+        plots_top_row = 3
+        ax = plt.subplot(2, plots_top_row, 3)
+        ax.set_title('Batteries')
+        ax.set(ylabel='Stored power in kWh')
+        for name, values in scenario.batteryLevels.items():
+            ax.plot(xlabels, values, label=name)
+        ax.legend()
+    else:
+        plots_top_row = 2
+
+    # vehicles
+    ax = plt.subplot(2, plots_top_row, 1)
+    ax.set_title('Vehicles')
+    ax.set(ylabel='SoC')
+    lines = ax.plot(xlabels, scenario.socs)
+    # reset color cycle, so lines have same color
+    ax.set_prop_cycle(None)
+
+    ax.plot(xlabels, scenario.disconnect, '--')
+    if len(scenario.constants.vehicles) <= 10:
+        ax.legend(lines, sorted(scenario.constants.vehicles.keys()))
+
+    # charging stations
+    ax = plt.subplot(2, plots_top_row, 2)
+    ax.set_title('Charging Stations')
+    ax.set(ylabel='Power in kW')
+    lines = ax.step(xlabels, scenario.sum_cs, where='post')
+    if len(scenario.constants.charging_stations) <= 10:
+        ax.legend(lines, sorted(scenario.constants.charging_stations.keys()))
+
+    # total power
+    ax = plt.subplot(2, 2, 3)
+    ax.step(xlabels, list([sum(cs) for cs in scenario.sum_cs]), label="CS", where='post')
+    gc_ids = scenario.constants.grid_connectors.keys()
+    for gcID in gc_ids:
+        for name, values in scenario.loads[gcID].items():
+            ax.step(xlabels, values, label=name, where='post')
+    # draw schedule
+    if scenario.strat.uses_window:
+        for gcID, schedule in scenario.gcWindowSchedule.items():
+            if all(s is not None for s in schedule):
+                # schedule exists
+                window_values = [v * int(max(scenario.totalLoad[gcID])) for v in schedule]
+                ax.step(xlabels, window_values, label="window {}".format(gcID),
+                        linestyle='--', where='post')
+    if scenario.strat.uses_schedule:
+        for gcID, schedule in scenario.gcPowerSchedule.items():
+            if any(s is not None for s in schedule):
+                ax.step(xlabels, schedule, label="Schedule {}".format(gcID), where='post')
+
+    ax.step(xlabels, scenario.all_totalLoad, label="Total", where='post')
+    ax.set_title('Power')
+    ax.set(ylabel='Power in kW')
+    ax.legend()
+    ax.xaxis_date()  # xaxis are datetime objects
+
+    # price
+    ax = plt.subplot(2, 2, 4)
+    prices = list(zip(*scenario.prices.values()))
+    lines = ax.step(xlabels, prices, where='post')
+    ax.set_title('Price for 1 kWh')
+    ax.set(ylabel='€')
+    if len(gc_ids) <= 10:
+        ax.legend(lines, sorted(gc_ids))
+
+    # figure title
+    fig = plt.gcf()
+    fig.suptitle('Strategy: {}'.format(scenario.strat.description), fontweight='bold')
+
+    # fig.autofmt_xdate()  # rotate xaxis labels (dates) to fit
+    # autofmt removes some axis labels, so rotate by hand:
+    for ax in fig.get_axes():
+        ax.set_xlim(scenario.start_time, scenario.stop_time)
+        plt.setp(ax.get_xticklabels(), rotation=30, ha='right')
+
+    # set size of figure and save it
+    plt.gcf().set_size_inches(10, 10)
+    plt.savefig(args.output_directory / "run_overview.png")
+    plt.savefig(args.output_directory / "run_overview.pdf")
+    # plt.show()
+
+
 def generate(schedule, scenario, args):
 
     # generate csv out of vehicle's socs
@@ -72,6 +164,10 @@ def generate(schedule, scenario, args):
 
     # generate cs power overview
     generate_cs_power_overview(scenario, args)
+
+    # save plots as png and pdf
+    aggregate_global_results(scenario)
+    plot(scenario, args)
 
     rotation_infos = []
 
