5038a6ee912b1a66584d8198d4a8e9d4c8a403c0
Julian Brendel <Julian.Brendel@rl-institut.de>
2022-12-20 13:25:37 +0100

    move input_data in output into subfolder

--- ebus_toolbox/__main__.py
+++ ebus_toolbox/__main__.py
@@ -120,7 +120,8 @@ if __name__ == '__main__':
     # if folder doesnt exists, create folder.
     # needs to happen after set_options_from_config since
     # args.output_directory can be overwritten by config
-    args.output_directory.mkdir(parents=True, exist_ok=True)
+    args.output_directory_input = Path(args.output_directory) / "input_data"
+    args.output_directory_input.mkdir(parents=True, exist_ok=True)
 
     # copy input files to output to ensure reproducibility
     copy_list = [args.config, args.electrified_stations, args.vehicle_types]
@@ -129,9 +130,9 @@ if __name__ == '__main__':
     if args.cost_parameters_file is not None:
         copy_list.append(args.cost_parameters_file)
     for c_file in copy_list:
-        shutil.copy(str(c_file), str(args.output_directory / Path(c_file).name))
+        shutil.copy(str(c_file), str(args.output_directory_input / Path(c_file).name))
 
-    util.save_version(Path(args.output_directory / "program_version.txt"))
+    util.save_version(Path(args.output_directory_input / "program_version.txt"))
 
     # rename special options
     args.timing = args.eta
