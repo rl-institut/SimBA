942574a2de3e72541cebae401be8b99d0d98c99d
yplatow <yannick.platow@rl-institut.de>
2023-01-31 16:38:42 +0100

    buffer_times angepasst

--- ebus_toolbox/sensitivity_analysis.py
+++ ebus_toolbox/sensitivity_analysis.py
@@ -44,6 +44,12 @@ def get_buffer_times():
     delay['hour'] = delay['hour'].astype(str)
     delay['r'] = delay['r'].astype(int)
 
+    range_hour = ['0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9', '9-10', '10-11', '11-12',
+                  '12-13', '13-14', '14-15', '15-16', '16-17', '17-18', '18-19', '19-20', '20-21', '21-22',
+                  '22-23', '23-0']
+
+    delay['hour'] = range_hour
+
     # create dictionary for cfg file
 
     delay = dict(zip(delay.hour, delay.r))
