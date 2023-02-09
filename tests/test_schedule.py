67364fe6bb20fe29f380fc0aa1f6cddade9daecd
paul.scheer <Paul.Scheer@rl-institut.de>
2023-02-09 11:16:18 +0100

    Make use of the util.argparser creation in main

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -7,11 +7,12 @@ Otherwise https://docs.pytest.org/en/latest/ and https://docs.python.org/3/libra
 are also good support.
 """
 import pytest
-
+import src.scenario
 from tests.helpers import generate_basic_schedule
 import json
 import pathlib
-from ebus_toolbox import schedule, trip, consumption
+from ebus_toolbox import schedule, trip, consumption, util
+
 
 test_root = pathlib.Path(__file__).parent
 file_root = test_root / "test_input_files"
@@ -58,9 +59,9 @@ class TestSchedule:
 
         path_to_all_station_data = file_root / "all_stations_example.csv"
         generated_schedule = schedule.Schedule.from_csv(path_to_trips, self.vehicle_types,
-                               self.electrified_stations, **mandatory_args,
-                               station_data_pata=path_to_all_station_data)
-        assert generated_schedule.stations
+                                                        self.electrified_stations, **mandatory_args,
+                                                        station_data_path=path_to_all_station_data)
+        assert generated_schedule.station_data is not None
 
         path_to_all_station_data = file_root / "not_existent_file"
         with pytest.warns(Warning) as record:
@@ -72,14 +73,83 @@ class TestSchedule:
             schedule.Schedule.from_csv(path_to_trips, self.vehicle_types, self.electrified_stations,
                                        **mandatory_args, station_data_path=path_to_all_station_data)
             assert len(record) == 2
-            assert 0
 
+        del trip.Trip.consumption
 
     def test_rotation_consistency(self):
-        pass
+        trip.Trip.consumption = consumption.Consumption(self.vehicle_types,
+                                                        outside_temperatures=self.temperature_path,
+                                                        level_of_loading_over_day=self.lol_path)
+        output_dir = test_root / "test_output_files"
+        rotation_consistency_dict = dict(check_rotation_consistency=True,
+                                         ignore_inconsistent_rotations=True,
+                                         output_directory=output_dir)
+
+        path_to_trips = file_root / "trips.csv"
+        generated_schedule = schedule.Schedule.from_csv(path_to_trips, self.vehicle_types,
+                                                        self.electrified_stations, **mandatory_args)
+
+        generated_schedule2 = schedule.Schedule.from_csv(path_to_trips, self.vehicle_types,
+                                                        self.electrified_stations, **mandatory_args,
+                                                        **rotation_consistency_dict)
+
+        assert len(generated_schedule.rotations) == len(generated_schedule2.rotations)
+
+
+
+
+        path_to_trips = file_root / "trips_no_einsetz.csv"
+        generated_schedule = schedule.Schedule.from_csv(path_to_trips, self.vehicle_types,
+                                                        self.electrified_stations, **mandatory_args,
+                                                        **rotation_consistency_dict)
+        assert len(generated_schedule.rotations) == 0
+
+        path_to_trips = file_root / "trips_no_aussetz.csv"
+        generated_schedule = schedule.Schedule.from_csv(path_to_trips, self.vehicle_types,
+                                                        self.electrified_stations, **mandatory_args,
+                                                        **rotation_consistency_dict)
+        assert len(generated_schedule.rotations) == 0
+
+        path_to_trips = file_root / "inconsistent_stations_trips.csv"
+        generated_schedule = schedule.Schedule.from_csv(path_to_trips, self.vehicle_types,
+                                                        self.electrified_stations, **mandatory_args,
+                                                        **rotation_consistency_dict)
+        assert len(generated_schedule.rotations) == 0
+
+        with pytest.warns(Warning) as record:
+            path_to_trips = file_root / "inconsistent_time_trips.csv"
+            generated_schedule = schedule.Schedule.from_csv(path_to_trips, self.vehicle_types,
+                                                            self.electrified_stations,
+                                                            **mandatory_args,
+                                                            check_rotation_consistency=False,
+                                                            ignore_inconsistent_rotations=True,
+                                                            output_directory=output_dir)
+            assert len(record) == 1
+
 
     def test_run(self):
-        pass
+        import argparse
+        parser = util.create_ArgumentParser_with_arguments()
+        args = parser.parse_args()
+        args.config = file_root / "ebus_toolbox.cfg"
+        args.days = None
+        args.seed = 5
+
+
+        trip.Trip.consumption = consumption.Consumption(self.vehicle_types,
+                                                        outside_temperatures=self.temperature_path,
+                                                        level_of_loading_over_day=self.lol_path)
+
+        path_to_all_station_data = file_root / "all_stations_example.csv"
+        generated_schedule = schedule.Schedule.from_csv(path_to_trips, self.vehicle_types,
+                                                        self.electrified_stations, **mandatory_args,
+                                                        station_data_path=path_to_all_station_data)
+
+        util.set_options_from_config(args, check=False, verbose=False)
+        scen = generated_schedule.run(args)
+
+        assert type(scen)==src.scenario.Scenario()
+
 
     def test_assign_vehicles(self):
         pass
@@ -107,5 +177,6 @@ class TestSchedule:
         assert len(generated_schedule.rotations) == 1
         assert type(generated_schedule) == schedule.Schedule
 
-t =TestSchedule()
-t.test_station_data_reading()
+
+t = TestSchedule()
+t.test_run()
