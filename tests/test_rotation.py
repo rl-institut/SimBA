dd61d433c64b0e97cc98e18447a3d7ad9fee47cf
paul.scheer <Paul.Scheer@rl-institut.de>
2023-04-12 16:18:21 +0200

    Remove unnecessary pytest docstring

--- tests/test_rotation.py
+++ tests/test_rotation.py
@@ -1,11 +1,3 @@
-"""
-run these tests with `pytest tests/test_something.py` or `pytest tests` or simply `pytest`
-pytest will look for all files starting with "test_" and run all functions
-within this file. For basic example of tests you can look at our workshop
-https://github.com/rl-institut/workshop/tree/master/test-driven-development.
-Otherwise https://docs.pytest.org/en/latest/ and https://docs.python.org/3/library/unittest.html
-are also good support.
-"""
 from tests.helpers import generate_basic_schedule
 
 
