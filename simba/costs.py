c4cebcdc3044ef8c5c870ae914abca87a78f0305
paul.scheer <Paul.Scheer@rl-institut.de>
2024-09-17 13:07:09 +0200

    Rename strategy to cost_strategy

--- simba/costs.py
+++ simba/costs.py
@@ -324,8 +324,8 @@ class Costs:
             # If no value is set, use the same strategy as the charging strategy
             default_cost_strategy = vars(self.args)["strategy_" + station.get("type")]
 
-            strategy_name = "cost_calculation_strategy_" + station.get("type")
-            cost_calculation_strategy = vars(self.args).get(strategy_name) or default_cost_strategy
+            cost_strategy_name = "cost_calculation_strategy_" + station.get("type")
+            cost_calculation_strategy = vars(self.args).get(cost_strategy_name) or default_cost_strategy
 
             # calculate costs for electricity
             try:
