59ff0e4ac29809e57a2f2759c2c2ab85d6012e74
stefansc1 <stefan.schirmeister@localiser.de>
2026-01-19 12:10:15 +0100

    plot language
    add option to translate extended plots to German

---
+++ simba/translate.py
@@ -0,0 +1,48 @@
+language = None
+translation_tables = {
+    "de": {
+        "Active Rotations": "Aktive Umläufe",
+        "bat. stored energy [kWh]": "stat. Speicher Energieinhalt [kWh]",
+        "battery power [kW]": " stat. Speicher Leistung [kW]",
+        "Block": "Umlauf",
+        "Charging type": "Ladetyp",
+        "Depot": "Depot",
+        "Distance [km]": "Distanz [km]",
+        "Distribution of energy consumption of rotations per vehicle type":
+            "Verteilung des Energiebedarfs über Umläufe und Fahrzeugtypen",
+        "Distribution of rotation length per vehicle type":
+            "Verteilung der Umlauflänge je Fahrzeugtyp",
+        "Energy consumption [kWh]": "Energiebedarf [kWh]",
+        "Feasibility of rotations per charging type": "Machbarkeit der Umläufe je Ladetyp",
+        "fixed load [kW]": "Feste Last [kW]",
+        "grid supply [kW]": "Netzanschlusspunkt [kW]",
+        "Inside window": "Innerhalb des HLZ",
+        "local generation [kW]": "lokale Erzeugung [kW]",
+        "negative rotations": "Negative Umläufe",
+        "Number of active rotations": "Anzahl aktiver Umläufe",
+        "Number of rotations": "Anzahl Umläufe",
+        "Opportunity": "Gelegenheit",
+        "Outside window": "Außerhalb des HLZ",
+        "Power": "Leistung",
+        "price [ct/kWh]": "Strompreis [ct/kWh]",
+        "stored battery energy [kWh]": "Batterie Energieinhalt [kWh]",
+        "successful rotations": "Erfolgreiche Umläufe",
+        "sum CS power [kW]": "Ladestationen [kW]",
+        "Vehicle ID": "Fahrzeug ID",
+    },
+}
+
+
+def set_language(new_language):
+    global language
+    language = new_language
+
+
+def translate(text):
+    if language in translation_tables:
+        # language set and known
+        table = translation_tables[language]
+        # find text in translation table, otherwise return text unchanged
+        return table.get(text, text)
+    # language not set or not known
+    return text
