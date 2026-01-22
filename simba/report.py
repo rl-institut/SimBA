4414cec15090747e8e04d7559ba439b7d4317cfb
Julian Brendel <Julian.Brendel@rl-institut.de>
2026-01-22 08:27:24 +0100

    add comments to facilitate plotting weekdays

--- simba/report.py
+++ simba/report.py
@@ -627,7 +627,11 @@ def plot_gc_power_timeseries(extended_plots_path, scenario, schedule, args):
 
         # find time column
         time_index = agg_ts["header"].index("time")
-        time_values = [row[time_index] for row in agg_ts["timeseries"]]
+        time_values = [row[time_index]-datetime for row in agg_ts["timeseries"]]
+
+        # This can be used if weekdays shall be plottet to start on a specific weekday
+        # date_corrector = agg_ts["timeseries"][0][1].weekday() - 0  # 0 stands for monday as a first day to plot
+        # time_values = [row[time_index]-datetime.timedelta(days=date_corrector) for row in agg_ts["timeseries"]]
 
         for header_index, header in enumerate(headers):
             try:
@@ -751,7 +755,7 @@ def plot_gc_power_timeseries(extended_plots_path, scenario, schedule, args):
         ax.set_xlim(time_values[0], time_values[-1])
         if args.plot_language == "de":
             ax.xaxis.set_major_formatter(mdates.DateFormatter("%d.%m.%Y"))
-            # ax.xaxis.set_major_formatter(mdates.DateFormatter("%A %H:%M"))  # Plots name of weekday and time
+        # ax.xaxis.set_major_formatter(mdates.DateFormatter("%A %H:%M"))  # Plots name of weekday and time
         ax.tick_params(axis="x", rotation=30)
         plt.tight_layout()
         plt.savefig(extended_plots_path / f"{sanitize(gcID)}_power_overview.png", dpi=DPI)
